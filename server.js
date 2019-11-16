0d3128631991f90251352b3b507a1aaa8cd97bf1
Roman Sydoruk <sydoruk89@gmail.com>
2019-11-15 23:42:35 -0800

    server

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(cors());
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
 app.get('/trails', getTrails);
+app.get('/movies', getMovies);
+app.get('/yelp', getYelp);
 
 
 app.get('*', (request, response) => {
@@ -41,7 +43,7 @@ function locationHandler(request, response) {
   }
 }
 
-//API routes
+// weather handler
 function weatherHandler(request, response) {
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
   // console.log('lat/long', request.query.data.latitude);
@@ -74,6 +76,38 @@ function getTrails(request, response) {
     });
 }
 
+// Movies handler
+
+function getMovies(request, response){
+  const url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.MOVIES_API_KEY}&query=${request.query.data.search_query}`;
+  superagent.get(url)
+    .then(data => {
+      console.log(data.body.results);
+      const moviesSummaries = data.body.results.slice(0, 20).map(element => {
+        return new Movies(element);
+      });
+      response.status(200).send(moviesSummaries);
+    })
+    .catch(() => {
+      errorHandler('Something went wrong', request, response);
+    });
+}
+
+// yelp handler
+function getYelp(request, response){
+  const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
+  superagent.get(url).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then(data => {
+      console.log(data.body.business);
+      const restaurantSummaries = data.body.businesses.slice(0, 20).map(element => {
+        return new Yelp(element);
+      });
+      response.status(200).send(restaurantSummaries);
+    })
+    .catch(() => {
+      errorHandler('Something went wrong', request, response);
+    });
+}
 
 // error handler
 function errorHandler(error, request, response) {
@@ -106,6 +140,27 @@ function Trails(trail){
   this.conditions = trail.conditionStatus;
   this.condition_date = trail.conditionDate.slice(0, 10);
   this.condition_time = trail.conditionDate.slice(11);
+}
+
+// movies constructor
+function Movies(movie){
+  this.title = movie.title;
+  this.released_on = movie.release_date;
+  this.total_votes = movie.vote_count;
+  this.average_votes = movie.vote_average;
+  this.popularity = movie.popularity;
+  this.image_url = `https://image.tmdb.org/t/p/w400${movie.backdrop_path}`;
+  this.overview = movie.overview;
+}
+
+// yelp constructor
+
+function Yelp(business){
+  this.name = business.name;
+  this.image_url = business.image_url;
+  this.price = business.price;
+  this.rating = business.rating;
+  this.url = business.url;
 }
 // //Ensure the server is listening for requests
 // // THIS MUST BE AT THE BOTTOM OF THE FILE!!!!
